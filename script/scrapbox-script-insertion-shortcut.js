b7b041a769eec315bc72143a2b0609543b4698bc
pollenJP <polleninjp@gmail.com>
2021-08-07 13:38:43 +0900

    Add scrapbox-script-insertion-shortcut.js

---
+++ script/scrapbox-script-insertion-shortcut.js
@@ -0,0 +1,28 @@
+function insertText(text) {
+  const cursor = document.getElementById("text-input");
+  cursor.focus();
+  const start = cursor.selectionStart; // in this case maybe 0
+  cursor.setRangeText(text);
+  cursor.selectionStart = cursor.selectionEnd = start + text.length;
+  const uiEvent = document.createEvent("UIEvent");
+  uiEvent.initEvent("input", true, false);
+  cursor.dispatchEvent(uiEvent);
+}
+
+// Alt + key //
+(() => {
+  const aliases = {
+    KeyR: function () {
+      insertText("[parent.icon]");
+    },
+  };
+
+  const onKeyDown = function (e) {
+    if (e.altKey && e.code in aliases) {
+      e.preventDefault();
+      aliases[e.code]();
+    }
+  };
+
+  document.addEventListener("keydown", onKeyDown);
+})();
