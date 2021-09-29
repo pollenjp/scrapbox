0c04c3feb9fdd934d00786d3e1ffe5c708944de7
pollenjp <polleninjp@gmail.com>
2021-09-29 20:39:01 +0900

    Add scrapbox-script-popup.js

---
+++ script/scrapbox-script-popup.js
@@ -0,0 +1,12 @@
+import "../scrapbox-script-popup-shortcut/script.js";
+
+var tmp_name = "icon+label";
+scrapbox.PopupMenu.addButton({
+  title: tmp_name,
+  onClick: function (text) {
+    text = text.replace(/\[(.*)(?<!(\.icon))\]/g, "[$1.icon] [$1]");
+    return text;
+  },
+});
+
+scrapboxPopupShortcut(tmp_name, (e) => e.charCode == 0x7b /* "{" */);
