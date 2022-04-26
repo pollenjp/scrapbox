c9045b771c2763bc063152292c6f7750bdb45efb
pollenJP <polleninjp@gmail.com>
2022-04-26 10:12:00 +0900

    Scrap Book bookmarklet

---
+++ bookmarklet/scrap_web_page_as_book.js
@@ -0,0 +1,65 @@
+javascript: (function () {
+  let project_name = "pollenJP-MEMO";
+  let comment = "comment variable";
+  let lines = [
+    "【Book】『』",
+    "",
+    " 出版社",
+    "  TODO: write here",
+    " x",
+    "  #Purchased",
+    "  #Unpurchased",
+    " #Book [Book.icon]",
+  ];
+
+
+  comment = "// Get the Web Page Information //";
+
+  let href = window.location.href;
+  let title = window.prompt("Bookmark to Scrapbox", document.title);
+  if (title == null) return;
+  title = title.replaceAll("[", "").replaceAll("]", "");
+
+  comment = "// add link //";
+
+  let document_title_str = document.title
+    .replaceAll("[", "")
+    .replaceAll("]", "");
+  lines.push(document_title_str);
+  lines.push("[" + href + "]");
+
+  comment = "// 引用文追加 //";
+
+  var quote = window.getSelection().toString();
+  if (quote.trim()) {
+    lines = lines.concat(
+      quote.split(/\n/g).map(function (line) {
+        if (line !== "") {
+          return "  > " + line;
+        }
+      })
+    );
+  }
+
+  comment = "// 空白行の削除 //";
+
+  let lines2 = [];
+  for (var i = 0; i < lines.length; ++i) {
+    if (lines[i] !== undefined) {
+      lines2.push(lines[i]);
+    }
+  }
+  lines2.push("");
+  var body = encodeURIComponent(lines2.join("\n"));
+
+  comment = "// Open the page //";
+
+  window.open(
+    "https://scrapbox.io/" +
+    encodeURIComponent(project_name) +
+    "/" +
+    encodeURIComponent(title.trim()) +
+    "?body=" +
+    body
+  );
+})();
