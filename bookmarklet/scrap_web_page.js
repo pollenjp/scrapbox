75c1fc338af3617459984887a0a04127df1292bc
pollenJP <polleninjp@gmail.com>
2022-08-01 12:31:40 +0900

    Add scrap to date

--- bookmarklet/scrap_web_page.js
+++ bookmarklet/scrap_web_page.js
@@ -5,6 +5,20 @@ javascript: (function () {
 
   comment = "// Get the Web Page Information //";
 
+  Date.prototype.format = function (format) {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return format
+      .replace("ddd", days[this.getDay()])
+      .replace("yyyy", this.getFullYear())
+      .replace("MM", ("0" + (this.getMonth() + 1)).slice(-2))
+      .replace("dd", ("0" + this.getDate()).slice(-2))
+      .replace("hh", ("0" + this.getHours()).slice(-2))
+      .replace("mm", ("0" + this.getMinutes()).slice(-2))
+      .replace("ss", ("0" + this.getSeconds()).slice(-2));
+  };
+
+  const date = new Date();
+
   let href = window.location.href;
   let title = window.prompt("Bookmark to Scrapbox", document.title);
   if (title == null) return;
@@ -28,7 +42,11 @@ javascript: (function () {
     .replaceAll("[", "")
     .replaceAll("]", "");
 
-  lines.push("[" + window.location.hostname + "]", "");
+  lines.push(
+    "[" + window.location.hostname + "]",
+    "Scrap at [" + date.format("yyyy-MM-dd") + "]",
+    ""
+  );
   lines.push(document_title_str);
   lines.push("[" + href + "]");
 
