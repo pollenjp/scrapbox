8e797d2de499de5bf96d39173c6c3db525bc0c7b
pollenJP <polleninjp@gmail.com>
2022-08-09 10:41:59 +0900

    add date

--- bookmarklet/scrap_vocaloid_music_page.js
+++ bookmarklet/scrap_vocaloid_music_page.js
@@ -6,6 +6,24 @@ javascript: (function () {
 
   comment = "// Get the Web Page Information //";
 
+  Date.prototype.format = function (format) {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return format
+      .replace("ddd", days[this.getDay()])
+      .replace("yyyy", this.getFullYear())
+      .replace("MM", ("0" + (this.getMonth() + 1)).slice(-2))
+      .replace("dd", ("0" + this.getDate()).slice(-2))
+      .replace("hh", ("0" + this.getHours()).slice(-2))
+      .replace("mm", ("0" + this.getMinutes()).slice(-2))
+      .replace("ss", ("0" + this.getSeconds()).slice(-2));
+  };
+
+  const date = new Date();
+
+  body_lines.push("Scrap at [date" + date.format("yyyy-MM-dd") + "]");
+
+  comment = "// Get the Web Page Information //";
+
   let href = window.location.href;
   let title = window.prompt("Bookmark to Scrapbox", document.title);
   if (title == null) return;
